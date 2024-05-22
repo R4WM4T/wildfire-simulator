1e2e0048868cfafd3d0d5d2c330a74975f8e55d5
Matthieu Tirloy <matthieu.tirloy@epita.fr>
2024-05-23 00:22:57 +0200

    fix(main): usage  message

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
     if (argc < 2)
     {
-        fprintf(stderr, "Usage: ./%s <input_file>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
         return -1;
     }
 
